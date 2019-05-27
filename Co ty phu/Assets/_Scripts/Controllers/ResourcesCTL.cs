125eca8472dc7ee202c5a27d84ce493325e0f4d4
LƯU NGUYỄN HOÀNG <luuhoang231@gmail.com>
2019-05-27 21:31:43 +0700

    #update
    Set up folder and create model class

---
+++ Co ty phu/Assets/_Scripts/Controllers/ResourcesCTL.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourcesCTL 
+{
+    private static ResourcesCTL _instance = null;
+    public static ResourcesCTL Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = new ResourcesCTL();
+            return _instance;
+        }
+    }
+
+    #region thuộc tính
+    private Material _redPlotMaterial;
+    public Material RedPlotMaterial
+    {
+        get
+        {
+            if(_redPlotMaterial == null)
+            {
+                _redPlotMaterial = Resources.Load<Material>("Materials/RedPlot");//load material
+            }
+            return _redPlotMaterial;
+        }
+    }
+    private Material _greenPlotMaterial;
+    public Material GreenPlotMaterial
+    {
+        get
+        {
+            if (_greenPlotMaterial == null)
+            {
+                _greenPlotMaterial = Resources.Load<Material>("Materials/GreenPlot");//load material
+            }
+            return _greenPlotMaterial;
+        }
+    }
+    #endregion 
+    
+}
---
+++ Co ty phu/Assets/_Scripts/Models/Plot.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plot : MonoBehaviour
+{
+    private EPlotColor _color;
+    public EPlotColor Color
+    {
+        get { return _color; }
+        set
+        {
+            _color = value;
+
+            switch (_color)
+            {
+                case EPlotColor.RED:
+                    GetComponent<Renderer>().material = ResourcesCTL.Instance.RedPlotMaterial;
+                    break;
+                case EPlotColor.GREEN:
+                    GetComponent<Renderer>().material = ResourcesCTL.Instance.GreenPlotMaterial;
+                    break;
+                case EPlotColor.BLUE:
+                    break;
+                case EPlotColor.YELLOW:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    [ContextMenu("ChangePlotColor")]
+    public void SetColor()
+    {
+        Color = EPlotColor.GREEN;
+    }
+}
